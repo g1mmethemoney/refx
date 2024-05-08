3870f4c145df73b7a662a31ca6dec356d4b3daee
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-05-08 17:47:39 +0300

    pass constructor arguments to OnConstruct

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,27 @@
-export declare abstract class BaseEffect {
-	protected OnStart(): void;
-	protected OnConstruct(): void;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type Signal from "@rbxts/signal";
+
+export declare abstract class BaseEffect<T extends unknown[] = []> {
+	public readonly Destroyed: Signal<() => void>;
+	public readonly IsDestroyed: boolean;
+
+	protected DestroyOnEnd: boolean;
+	protected DestroyOnLifecycleEnd: boolean;
+	protected MaxLifetime: number;
+	protected DisableLeakWarning: boolean;
+
+	constructor(...args: T);
+
+	/** @server */
+	Start(players: Player[]): this;
+	/** @server */
+	GetPlayers(): Player[];
+	/** @server */
+	Destroy(): this;
+
+	protected readonly Configuration: T;
+	protected OnStart(...args: T): void;
+	protected OnConstruct(...args: T): void;
 	protected OnDestroy(): void;
 }
 
@@ -10,7 +31,7 @@ export interface FunctionAttributes {
 	Unreliable: boolean;
 }
 
-export declare function VisualEffectDecorator<T extends Constructor<BaseEffect>>(ctor: T): T;
+export declare function VisualEffectDecorator<T extends Constructor<BaseEffect<any[]>>>(ctor: T): T;
 export declare function Config(
 	attributes: Partial<FunctionAttributes>,
-): <T extends BaseEffect>(ctor: T, methodName: string) => void;
+): <T extends BaseEffect<any[]>>(ctor: T, methodName: string) => void;
